5cc47ee7ddab0322677f12a4213b155f65725c04
v12581234 <vinhnguyen125812@gmail.com>
2025-06-05 15:05:00 +0700

    Commt 02: add 2 new lines

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("Commit 01");
+        System.out.println("Commit 02");
+
     }
 }
